5d1c96a9858e76ba0527a66b579c036696c9f8fb
ohyama-hinata <gp20a023@oecu.jp>
2024-06-22 01:25:26 +0000

    fix absolute pass through

---
+++ components/layouts/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="text-white">
+      header
+      <div className="placeholder:">header</div>
+    </header>
+  );
+};
+
+export default Header;
